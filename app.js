731d4218f9c1da9fa7ce1bf4034dffb4d427e463
RajTechnosoft <136947707+RajTechnosoft@users.noreply.github.com>
2024-04-22 23:44:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// const hiddenElements = document.querySelectorAll('.hidden');
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('hidden')
+//             entry.target.classList.add('show')
+//         } else{
+//             entry.target.classList.remove('show')
+//         }
+//     })
+// })
+
+// console.log("hii")
+// console.log(entry)
